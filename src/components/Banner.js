93ccf4a85153cc75cb9010d59e4775db9f010755
mjaydenkim <mjaydenkim@gmail.com>
2019-08-15 13:18:17 -0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,8 +7,8 @@ const Banner = (props) => (
                 <h1>Submit your Egan schedules here!</h1>
             </header>
             <div className="content">
-                <p>Site built with GatsbyJS and React<br />
-                using the template Forty by HTML5UP..</p>
+                <p>Site built by Matthew Kim with GatsbyJS and React<br />
+                using the template Forty by HTML5UP.</p>
                 <ul className="actions">
                     <li><a href="/elements" className="button next scrolly">Get Started</a></li>
                 </ul>
