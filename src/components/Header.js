6978bf59640e8f52f2164ba711638460c0883f0b
mjaydenkim <mjaydenkim@gmail.com>
2019-08-15 15:59:25 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="https://www.youtube.com/watch?v=6n3pFFPSlW4" className="logo"><strong>Egan</strong> <span>Schedules</span></Link>
+        <a href="https://www.youtube.com/watch?v=6n3pFFPSlW4"><strong>Egan</strong> <span>Schedules</span></a>
     </header>
 )
 
